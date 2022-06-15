c4b88641e1fa94e5a3b477906c1e53ea8e327650
dimitris <dimitrisg@moveo.ai>
2022-06-15 16:12:17 +0300

    adding new problem solutions

---
+++ hackerrank/a_tale_of_two_stacks/two_stacks.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sz(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define reset(a,b) memset(a,b,sizeof(a))
+#define pb push_back
+#define Foreach(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define For(i, j) FOR(i, 0, j, 1)
+#define Rfor(i, j) RFOR(i, j, 0, 1)
+
+
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef map<int,int> mpii;
+typedef set<int> seti;
+
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+class MyQueue {
+
+    public:
+        stack<int> stack_newest_on_top, stack_oldest_on_top;
+        void push(int x) {
+            stack_oldest_on_top.push(x);
+            cout << "Pushed " << x << endl;
+        }
+
+        void move_old_to_new () {
+            while (! stack_oldest_on_top.empty()) {
+                stack_newest_on_top.push(stack_oldest_on_top.top());
+                stack_oldest_on_top.pop();
+            }
+            cout << "Moved elements to the other stack" << endl;
+        }
+
+        void pop() {
+            if (! stack_newest_on_top.empty()) {
+                cout << "Popped " << stack_newest_on_top.top() << endl;
+                stack_newest_on_top.pop();
+                return;
+            }
+            move_old_to_new();
+            if (! stack_newest_on_top.empty()) {
+                cout << "Popped " << stack_newest_on_top.top() << endl;
+                stack_newest_on_top.pop();
+            }
+        }
+
+        int front() {
+            if (stack_newest_on_top.empty()) {
+                move_old_to_new();
+                return stack_newest_on_top.top();
+            } else {
+                return stack_newest_on_top.top();
+            }
+        }
+};
+
+int main() {
+    MyQueue q1;
+    int q, type, x;
+    cin >> q;
+
+    for(int i = 0; i < q; i++) {
+        cin >> type;
+        if(type == 1) {
+            cin >> x;
+            q1.push(x);
+        }
+        else if(type == 2) {
+            q1.pop();
+        }
+        else cout << q1.front() << endl;
+    }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
